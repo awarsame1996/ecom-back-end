dea53ac839ba5477fdf173f51594a87f37cc61a2
Your Name <you@example.com>
2022-08-01 20:10:29 +0100

    update

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 	/* req.body should look like this...
     {
       product_name: "Basketball",
@@ -70,7 +70,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
 	// update product data
 	Product.update(req.body, {
 		where: {
@@ -104,7 +104,7 @@ router.put('/:id', (req, res) => {
 				ProductTag.bulkCreate(newProductTags),
 			]);
 		})
-		.then((updatedProductTags) => res.json(updatedProductTags))
+		.then((updatedProductTags) => res.status(200).json(updatedProductTags))
 		.catch((err) => {
 			// console.log(err);
 			res.status(400).json(err);
